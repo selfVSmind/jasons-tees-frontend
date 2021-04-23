23ddddf7cd62344c7e9331a9512cf8af31657340
Jason Lambert <pitbullent@gmail.com>
2021-04-23 12:26:19 -0700

    fixed some responsiveness issues with 'select a graphic' dialog on mobile

--- src/app/create-shirt/cards/graphic-card/graphic-card.component.ts
+++ src/app/create-shirt/cards/graphic-card/graphic-card.component.ts
@@ -1,5 +1,7 @@
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 import { GraphicCardDialogComponent } from './graphic-card-dialog/graphic-card-dialog.component';
 
 interface CncCutFile {
@@ -15,7 +17,12 @@ interface CncCutFile {
 })
 export class GraphicCardComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) {}
+  isXSmall: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.XSmall);
+
+  constructor(
+    public dialog: MatDialog,
+    private breakpointObserver: BreakpointObserver
+  ) {}
 
   ngOnInit(): void {
   }
@@ -26,11 +33,26 @@ export class GraphicCardComponent implements OnInit {
     let dialogRef = this.dialog.open(
       GraphicCardDialogComponent,
       {
-        width: '850px',
-        height: '650px',
-      });
+        // width: '850px',
+        // height: '650px',
+        width: '75%',
+        height: '75%',
+        maxWidth: '100vw',
+        maxHeight: '100vh'
+      }
+    );
 
+    const smallDialogSubscription = this.isXSmall.subscribe(result => {
+      if(result.matches) {
+        // width, height
+        dialogRef.updateSize('90%', '90%');
+      } else {
+        dialogRef.updateSize('75%', '75%');
+      }
+    })
+    
     dialogRef.afterClosed().subscribe(cutFile => {
+      smallDialogSubscription.unsubscribe();
       this.cutFile = cutFile;
       console.log(cutFile.name, "graphic has been selected.")
     });
