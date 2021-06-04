da4983fd8d17dc3947f021f0b70f7dca6a10e880
Jason Lambert <pitbullent@gmail.com>
2021-06-04 07:41:33 -0700

    Revert "disabled empty designs page"
    This reverts commit beaefe2685316c095f505cb836fc82e46d936195.
    
    I didn't care for the autoformatting of my code editor

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,17 +6,18 @@ import { map, shareReplay } from 'rxjs/operators';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss'],
+  styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
-  menuItems = ['about', 'create', 'inventory', 'settings'];
 
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe('(max-width: 960px)')
+export class NavComponent {
+  menuItems = ['about', 'create', 'inventory', 'designs', 'settings'];
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe('(max-width: 960px)')
     .pipe(
-      map((result) => result.matches),
+      map(result => result.matches),
       shareReplay()
     );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
+
 }
